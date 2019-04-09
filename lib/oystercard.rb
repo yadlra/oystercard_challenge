0c3016b7122f0c4c74b7293693ecdc12cd7a1486
Stuart <stuart.pethurst@gmail.com>
2019-04-09 17:12:42 +0100

    created a deduct method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -10,4 +10,8 @@ def top_up(value)
   @balance = @balance + value
   raise "Maximum balance exceeded" if @balance + value > MAXIMUM_BALANCE
 end
+
+def deduct(value)
+  @balance -= value
+end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -20,4 +20,14 @@ describe "#top_up" do
     expect{ subject.top_up 1 }.to raise_error "Maximum balance exceeded"
 end
 end
+
+describe "#deduct" do
+
+  it { is_expected.to respond_to(:deduct).with(1).argument }
+
+  it 'deducts an amount from the balance' do
+    subject.top_up(20)
+    expect{ subject.deduct 1}.to change{ subject.balance }.by -1
+  end
+end
 end
