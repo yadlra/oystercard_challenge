28c54c46de83b23c862a7c61cd0904c134999485
Stuart <stuart.pethurst@gmail.com>
2019-04-09 15:45:33 +0100

    created top_up method and max balance constant of 90

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,13 @@
 class Oystercard
-
+MAXIMUM_BALANCE = 90
   attr_reader :balance
-  
+
  def initialize
    @balance = 0
  end
 
+def top_up(value)
+  @balance = @balance + value
+  raise "Maximum balance exceeded" if @balance + value > MAXIMUM_BALANCE
+end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,4 +5,19 @@ describe Oystercard do
   it 'checks card balance is zero' do
     expect(subject.balance).to eq (0)
   end
+
+describe "#top_up" do
+
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+  it " can top up the balance" do
+  expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+  end
+
+    it "raises an error if card balance is more then 90" do
+      MAXIMUM_BALANCE = Oystercard::MAXIMUM_BALANCE
+      subject.top_up(MAXIMUM_BALANCE)
+    expect{ subject.top_up 1 }.to raise_error "Maximum balance exceeded"
+end
+end
 end
