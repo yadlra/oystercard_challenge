85b94f27d529475f0144d884617d6dfab5915ae6
yadlra <yadi.sb1@gmail.com>
2019-04-11 15:17:19 +0100

    took deduct method out

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,36 +6,27 @@ describe Oystercard do
   it 'checks card balance is zero' do
     expect(subject.balance).to eq (0)
   end
-  
+
   describe "#top_up" do
 
     it { is_expected.to respond_to(:top_up).with(1).argument }
-    
+
     it " can top up the balance" do
       expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
     end
-    
+
     it "raises an error if card balance is more then 90" do
       maximum_balance = Oystercard::MAXIMUM_BALANCE
       subject.top_up(maximum_balance)
       expect { subject.top_up(1) }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
     end
-    
-    describe "#deduct" do
-      
-      it { is_expected.to respond_to(:deduct).with(1).argument }
-      
-      it 'deducts an amount from the balance' do
-      subject.top_up(20)
-      expect{ subject.deduct 1}.to change{ subject.balance }.by -1
-    end
   end
-  
+
   it 'starts not in journey' do
 
     expect(subject.in_journey).to eq false
   end
-end
+
 
 describe '#touch_in' do
   it 'shows when journey has started' do
@@ -50,9 +41,15 @@ describe '#touch_in' do
 end
 
 describe '#touch_out' do
-  
+
   it 'shows when journey has ended' do
-    expect(subject.touch_out).to eq false 
+    expect(subject.touch_out).to eq false
+  end
+
+  it 'charges when the card is touched out' do
+    subject.top_up(6)
+    subject.touch_in
+    expect {subject.touch_out}.to change{subject.balance}.by(- Oystercard::MIN_FARE)
   end
 end
-end
+end 
